6c12334052d0a07bc5e946723a1f343a868a7758
JacobKong <947124563@qq.com>
2015-12-09 22:40:13 +0800

    Jacob-完成主界面的天气、学习生活的layout

--- app/src/main/java/edu/neu/campusassistant/activity/MainActivity.java
+++ app/src/main/java/edu/neu/campusassistant/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package edu.neu.campusassistant;
+package edu.neu.campusassistant.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import edu.neu.campusassistant.R;
 
 public class MainActivity extends AppCompatActivity {
 
