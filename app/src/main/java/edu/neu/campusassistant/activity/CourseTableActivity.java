8774fb6fce867564f1a730d3e4394e884e06a4c3
JacobKong <947124563@qq.com>
2015-12-24 00:48:03 +0800

    Jacob-天气图标重命名并加入

--- app/src/main/java/edu/neu/campusassistant/activity/CourseTableActivity.java
+++ app/src/main/java/edu/neu/campusassistant/activity/CourseTableActivity.java
@@ -19,44 +19,7 @@ import java.util.Locale;
 import butterknife.Bind;
 import edu.neu.campusassistant.R;
 
-public class CourseTableActivity extends AppCompatActivity implements MonthLoader.MonthChangeListener{
-
-    @Bind(R.id.course_week_view)
-    WeekView mWeekView;
-
-    private WeekView.EventClickListener mEventClickListener;
-    private WeekView.EventLongPressListener mEventLongPressListener;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_course_table);
-
-        // Get a reference for the week view in the layout.
-        mWeekView = (WeekView) findViewById(R.id.course_week_view);
-
-        // The week view has infinite scrolling horizontally. We have to provide the events of a
-        // month every time the month changes on the week view.
-        mWeekView.setMonthChangeListener(this);
-
-//        // Set up a date time interpreter to interpret how the date and time will be formatted in
-//        // the week view. This is optional.
-//        setupDateTimeInterpreter(false);
-
-    }
-
-
-    @Override
-    public List<WeekViewEvent> onMonthChange(int newYear, int newMonth) {
-        // Populate the week view with some events.
-        List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
-
-        return events;
-    }
-
-    private String getEventTitle(Calendar time) {
-        return String.format("Event of %02d:%02d %s/%d", time.get(Calendar.HOUR_OF_DAY), time.get(Calendar.MINUTE), time.get(Calendar.MONTH)+1, time.get(Calendar.DAY_OF_MONTH));
-    }
+public class CourseTableActivity extends AppCompatActivity{
 
 
 }
