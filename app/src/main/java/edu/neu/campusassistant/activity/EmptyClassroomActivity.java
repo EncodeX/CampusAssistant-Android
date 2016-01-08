3213babb0d35544683623de20f9e97cbdcd43b36
Encode.X <encode.xii@gmail.com>
2016-01-08 23:31:59 +0800

    教学楼Spinner示例完成

--- app/src/main/java/edu/neu/campusassistant/activity/EmptyClassroomActivity.java
+++ app/src/main/java/edu/neu/campusassistant/activity/EmptyClassroomActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatSpinner;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import butterknife.Bind;
@@ -20,12 +21,21 @@ public class EmptyClassroomActivity extends AppCompatActivity {
 	@Bind(R.id.building_spinner)
 	AppCompatSpinner mBuildingSpinner;
 
+	BuildingListAdapter mBuildingListAdapter;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_empty_classroom);
 
 		initView();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		mBuildingListAdapter.refreshBuildingList();
 	}
 
 	private void initView() {
@@ -43,6 +53,19 @@ public class EmptyClassroomActivity extends AppCompatActivity {
 			}
 		});
 
-		mBuildingSpinner.setAdapter(new BuildingListAdapter(this));
+		mBuildingListAdapter = new BuildingListAdapter(this);
+		mBuildingSpinner.setAdapter(mBuildingListAdapter);
+
+		mBuildingListAdapter.setListRefreshListener(new BuildingListAdapter.ListRefreshListener() {
+			@Override
+			public void onRefreshListSucceeded() {
+
+			}
+
+			@Override
+			public void onRefreshListFailed() {
+				Log.d("EmptyClassroomActivity","获取教学楼列表失败");
+			}
+		});
 	}
 }
--- app/src/main/java/edu/neu/campusassistant/adapter/BuildingListAdapter.java
+++ app/src/main/java/edu/neu/campusassistant/adapter/BuildingListAdapter.java
@@ -1,6 +1,7 @@
 package edu.neu.campusassistant.adapter;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,12 +9,21 @@ import android.widget.BaseAdapter;
 import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import edu.neu.campusassistant.R;
+import edu.neu.campusassistant.utils.AppController;
+import edu.neu.campusassistant.utils.Constants;
 
 /**
  * Created with Android Studio.
@@ -27,16 +37,21 @@ public class BuildingListAdapter extends BaseAdapter implements SpinnerAdapter {
 	private List<String> mBuildingNameList;
 	private List<String> mBuildingIdList;
 	private Context mContext;
+	private SharedPreferences mSharedPreferences;
+
+	private ListRefreshListener mListRefreshListener;
 
 	public BuildingListAdapter(Context context) {
 		this.mBuildingIdList = new ArrayList<>();
 		this.mBuildingNameList = new ArrayList<>();
 		this.mContext = context;
+
+		this.mSharedPreferences = context.getSharedPreferences(Constants.SHARED_PREFS_KEY, Context.MODE_PRIVATE);
 	}
 
 	@Override
 	public int getCount() {
-		return 10;
+		return mBuildingIdList.size();
 	}
 
 	@Override
@@ -53,15 +68,15 @@ public class BuildingListAdapter extends BaseAdapter implements SpinnerAdapter {
 	@Override
 	public View getView(int i, View view, ViewGroup viewGroup) {
 		ViewHolderItem viewHolderItem;
-		if(view ==null){
+		if (view == null) {
 			view = LayoutInflater.from(mContext).inflate(R.layout.building_spinner_item_selected, viewGroup, false);
 			viewHolderItem = new ViewHolderItem(view);
 			view.setTag(viewHolderItem);
-		}else {
-			viewHolderItem = (ViewHolderItem)view.getTag();
+		} else {
+			viewHolderItem = (ViewHolderItem) view.getTag();
 		}
 
-		viewHolderItem.buildingNameLabel.setText("TEST TEST TEST TEST"+i);
+		viewHolderItem.buildingNameLabel.setText(mBuildingNameList.get(i));
 
 		return view;
 	}
@@ -69,24 +84,78 @@ public class BuildingListAdapter extends BaseAdapter implements SpinnerAdapter {
 	@Override
 	public View getDropDownView(int position, View convertView, ViewGroup parent) {
 		ViewHolderItem viewHolderItem;
-		if(convertView == null){
+		if (convertView == null) {
 			convertView = LayoutInflater.from(mContext).inflate(R.layout.building_spinner_item, parent, false);
 			viewHolderItem = new ViewHolderItem(convertView);
 			convertView.setTag(viewHolderItem);
-		}else {
-			viewHolderItem = (ViewHolderItem)convertView.getTag();
+		} else {
+			viewHolderItem = (ViewHolderItem) convertView.getTag();
 		}
 
-		viewHolderItem.buildingNameLabel.setText("TEST TEST TEST TEST"+position);
+		viewHolderItem.buildingNameLabel.setText(mBuildingNameList.get(position));
 
 		return convertView;
 	}
 
-	static class ViewHolderItem{
-		@Bind(R.id.building_name_label)TextView buildingNameLabel;
+	public void setListRefreshListener(ListRefreshListener listRefreshListener) {
+		this.mListRefreshListener = listRefreshListener;
+	}
+
+	public void refreshBuildingList() {
+		final String token = mSharedPreferences.getString(Constants.AAO_TOKEN, "");
+
+		if (token.equals("")) {
+			if(mListRefreshListener != null) mListRefreshListener.onRefreshListFailed();
+			return;
+		}
+
+		JsonObjectRequest request = new JsonObjectRequest(
+				"http://202.118.31.241:8080/api/v1/buildings?token=" + token,
+				new BuildingJSONObjectListener(),
+				new Response.ErrorListener() {
+					@Override
+					public void onErrorResponse(VolleyError error) {
+						if(mListRefreshListener != null) mListRefreshListener.onRefreshListFailed();
+					}
+				}
+		);
+
+		AppController.getInstance().addToRequestQueue(request);
+	}
+
+	static class ViewHolderItem {
+		@Bind(R.id.building_name_label)
+		TextView buildingNameLabel;
 
 		public ViewHolderItem(View itemView) {
-			ButterKnife.bind(this,itemView);
+			ButterKnife.bind(this, itemView);
+		}
+	}
+
+	public interface ListRefreshListener{
+		void onRefreshListSucceeded();
+		void onRefreshListFailed();
+	}
+
+	private class BuildingJSONObjectListener implements Response.Listener<JSONObject> {
+		@Override
+		public void onResponse(JSONObject response) {
+			JSONArray data = response.optJSONArray("data");
+
+			if(data!=null){
+				mBuildingIdList.clear();
+				mBuildingNameList.clear();
+
+				for(int i=0; i<data.length(); ++i){
+					JSONObject object = data.optJSONObject(i);
+					if(object == null) continue;
+					mBuildingIdList.add(object.optString("buildingId"));
+					mBuildingNameList.add(object.optString("buildingName"));
+				}
+
+				if(mListRefreshListener != null) mListRefreshListener.onRefreshListSucceeded();
+				notifyDataSetChanged();
+			}
 		}
 	}
 }
